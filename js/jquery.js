d8480e1ec21269363f745fa1f76a0ecc2962c1e7
Karam H. J. Zomlut <63299107+karam-zomlut@users.noreply.github.com>
2021-03-27 22:26:57 +0300

    Update jquery.js

--- js/jquery.js
+++ js/jquery.js
@@ -1,18 +1,1 @@
-$(function () {
-    // Play Video.
-    $('.play-vid').click(function (e) { 
-        e.preventDefault();
-        $('.video-player').addClass('active');
-    });
-    // Close Video
-    $('.close-vid').click(function (e) { 
-        e.preventDefault();
-        $('.video-player').removeClass('active');
-    });
-
-    // Counter
-    $('.success-box .num').counterUp({
-        delay: 10,
-        time: 1100
-    })
-});
+$(function(){$('.play-vid').click(function(e){e.preventDefault();$('.video-player').addClass('active');});$('.close-vid').click(function(e){e.preventDefault();$('.video-player').removeClass('active');});$('.success-box .num').counterUp({delay:10,time:1100})});
